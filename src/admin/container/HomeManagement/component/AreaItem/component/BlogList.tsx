b5f6f7bce78b61e70a8e7d10f2b0f83287e6d0bd
Kungg <1592500694@qq.com>
2021-12-10 00:33:29 +0800

    fix:图片宽显示是错误

--- src/admin/container/HomeManagement/component/AreaItem/component/BlogList.tsx
+++ src/admin/container/HomeManagement/component/AreaItem/component/BlogList.tsx
@@ -1,10 +1,9 @@
 
 import styled from '@emotion/styled'
-import { Input,Button,Modal } from 'antd'
+import { Input,Button, } from 'antd'
 import { Schema } from 'common/type';
-import { useState } from 'react';
 export const List =(props:any)=>{
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
 const {children=[],changeChildren} = props;
 const addItemToChildren =() =>{
   const newChildren =[...children];
@@ -37,18 +36,6 @@ const changeChildrenItem =(index: number,key: string | number,value: any)=>{
   changeChildren(newChildren)
 }
 
-const showModal = () => {
-  setIsModalVisible(true);
-};
-const handleModalOk = () => {
-  setIsModalVisible(false);
-  
-};
-const handleModalCancel = (index:any) => {
-  changeChildrenItem(index,'width','200px')
-  changeChildrenItem(index,'height','200px')
-  setIsModalVisible(false);
-};
 
   return (<Wrapper>
     <Button type="primary" style={{
@@ -79,18 +66,13 @@ const handleModalCancel = (index:any) => {
              ></Input>
            </Row>
            <Row>
-             <span onClick={showModal}>图片</span>
-             <Modal
-             title="设置图片高度和宽度"
-             visible={isModalVisible}
-             onOk={handleModalOk}
-             onCancel={()=>{handleModalCancel(index)}}
-               style={{position:'absolute',
-                  height:450,
-                  right: 10
-              }} 
-               >
-                <Input 
+             <span>图片</span>
+             <Input 
+              value={attributes?.imgeUrl} 
+             placeholder='请输入图片地址' 
+               onChange={(e)=>{changeChildrenItem(index,'imgeUrl',e.target.value)}}
+             ></Input>
+              <Input 
               value={attributes?.height} 
              placeholder='请输入图片高度' 
              defaultValue='200px'
@@ -101,16 +83,6 @@ const handleModalCancel = (index:any) => {
              placeholder='请输入图片宽度'
              defaultValue='200px'
                onChange={(e)=>{changeChildrenItem(index,'width',e.target.value)}}
-               style={{
-                marginTop:'10px'
-              }}
-             ></Input>
-
-             </Modal>
-             <Input 
-              value={attributes?.imgeUrl} 
-             placeholder='请输入图片地址' 
-               onChange={(e)=>{changeChildrenItem(index,'imgeUrl',e.target.value)}}
              ></Input>
            </Row>
            <Row>
